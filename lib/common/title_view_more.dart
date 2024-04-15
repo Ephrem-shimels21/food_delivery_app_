2556d2ae37210a8f3ee818b353a3f2911b5c79bb
Ephrem <eph21sz@gmail.com>
2024-04-15 15:59:31 +0300

    modify title_viewMore widget

--- lib/common/title_view_more.dart
+++ lib/common/title_view_more.dart
@@ -4,29 +4,34 @@ import 'package:food_delivery_appb/utils/color_extension.dart';
 class TitleAndViewMore extends StatelessWidget {
   final String title;
   final VoidCallback onView;
+  final bool isViewMore;
   const TitleAndViewMore(
-      {super.key, required this.title, required this.onView});
+      {super.key,
+      required this.title,
+      required this.onView,
+      required this.isViewMore});
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(title,
             style: TextStyle(
                 color: UniversalColors.primaryText,
                 fontSize: 18,
                 fontWeight: FontWeight.w800)),
-        TextButton(
-          onPressed: onView,
-          child: Text(
-            'View More',
-            style: TextStyle(
-                color: UniversalColors.primary,
-                fontSize: 14,
-                fontWeight: FontWeight.w600),
-          ),
-        )
+        if (isViewMore)
+          TextButton(
+            onPressed: onView,
+            child: Text(
+              'View More',
+              style: TextStyle(
+                  color: UniversalColors.primary,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600),
+            ),
+          )
       ],
     );
   }
