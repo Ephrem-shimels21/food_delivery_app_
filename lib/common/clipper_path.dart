0816bbd93904e1572d7d626bda87e9ef5b3307c7
Ephrem <eph21sz@gmail.com>
2024-04-15 09:28:41 +0300

    create separate file for clipper

---
+++ lib/common/clipper_path.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomClipperPath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
