29790c9cd9114bf6b5f3af593c3b27cb7cc089b6
Ephrem <eph21sz@gmail.com>
2024-04-19 10:05:11 +0300

    add new routes to food and restaurant detail pages

--- lib/Route/route.dart
+++ lib/Route/route.dart
@@ -5,6 +5,7 @@ import 'package:food_delivery_appb/auth/screens/otp_verfication.dart';
 import 'package:food_delivery_appb/auth/screens/password_reset.dart';
 import 'package:food_delivery_appb/auth/screens/signup.dart';
 import 'package:food_delivery_appb/common/bottom_navigation.dart';
+import 'package:food_delivery_appb/food/screens/food_detail.dart';
 import 'package:food_delivery_appb/home/Screens/filtering_page.dart';
 import 'package:food_delivery_appb/home/Screens/home_view.dart';
 import 'package:food_delivery_appb/home/Screens/popular_menus.dart';
@@ -12,6 +13,7 @@ import 'package:food_delivery_appb/home/Screens/popular_restaurants.dart';
 import 'package:food_delivery_appb/on_boarding/on_boarding_1.dart';
 import 'package:food_delivery_appb/on_boarding/on_boarding_2.dart';
 import 'package:food_delivery_appb/on_boarding/splash.dart';
+import 'package:food_delivery_appb/restaurant/screens/restaurant_detail.dart';
 import 'package:go_router/go_router.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -64,6 +66,24 @@ GoRouter router = GoRouter(
         parentNavigatorKey: _rootNavigatorKey,
         builder: (context, state) => const PasswordReset(),
       ),
+      GoRoute(
+          path: '/restaurantDetail/:name/:image',
+          parentNavigatorKey: _rootNavigatorKey,
+          pageBuilder: (context, state) {
+            final name = state.pathParameters['name'];
+            final image = state.pathParameters['image'];
+            return MaterialPage(
+                child: RestaurantDetail(title: name!, imagePath: image!));
+          }),
+      GoRoute(
+          path: '/foodDetail/:name/:image',
+          parentNavigatorKey: _rootNavigatorKey,
+          pageBuilder: (context, state) {
+            final name = state.pathParameters['name'];
+            final image = state.pathParameters['image'];
+            return MaterialPage(
+                child: FoodDetail(title: name!, imagePath: image!));
+          }),
       ShellRoute(
           navigatorKey: _shellNavigatorKey,
           builder: (context, state, child) => BottomBar(child: child),
