a2b7e20257528feb5864826564c22d79784732ee
Ephrem <eph21sz@gmail.com>
2024-04-22 15:23:30 +0300

    increase the icon size and center the card content

--- lib/notification/widgets/notification_card.dart
+++ lib/notification/widgets/notification_card.dart
@@ -10,26 +10,31 @@ class NotificationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Container(
+        height: 100,
         color: Colors.white,
-        elevation: 5.0,
-        child: ListTile(
-          leading: Icon(
-            notification['icon'],
-            color: notification['color'],
-          ),
-          title: Text(
-            notification['message'],
-            style: TextStyle(
-                fontWeight: FontWeight.w800,
-                color: UniversalColors.primaryText),
-          ),
-          subtitle: Text(
-            notification['time'],
-            style: TextStyle(
-                color: UniversalColors.secondaryText,
-                fontWeight: FontWeight.w400),
-          ),
-        ));
+        child: Card(
+            color: Colors.white,
+            elevation: 5.0,
+            child: Center(
+                child: ListTile(
+              leading: Icon(
+                notification['icon'],
+                color: notification['color'],
+                size: 50.0,
+              ),
+              title: Text(
+                notification['message'],
+                style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    color: UniversalColors.primaryText),
+              ),
+              subtitle: Text(
+                notification['time'],
+                style: TextStyle(
+                    color: UniversalColors.secondaryText,
+                    fontWeight: FontWeight.w400),
+              ),
+            ))));
   }
 }
